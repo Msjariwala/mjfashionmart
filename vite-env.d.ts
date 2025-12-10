f9ed221001014b4e08b232f6253fdfd9c016e666
Msjariwala <jariwalamohit2004@gmail.com>
2025-12-11 01:24:36 +0530

    update vit-env.d.ts file

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,10 +1,10 @@
 interface ImportMetaEnv {
-  readonly VITE_FIREBASE_API_KEY: string;
-  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
-  readonly VITE_FIREBASE_PROJECT_ID: string;
-  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
-  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
-  readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_FIREBASE_API_KEY: "AIzaSyCSvXcMiWH9LYk__1rgxR7YcVy6p5jE1CI";
+  readonly VITE_FIREBASE_AUTH_DOMAIN: "feedbackdata-c5591.firebaseapp.com";
+  readonly VITE_FIREBASE_PROJECT_ID: "feedbackdata-c5591";
+  readonly VITE_FIREBASE_STORAGE_BUCKET: "feedbackdata-c5591.firebasestorage.app";
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: "504210155858";
+  readonly VITE_FIREBASE_APP_ID: "1:504210155858:web:49485311977f903a06f28b";
   // Add more env variables here if needed
 }
 
